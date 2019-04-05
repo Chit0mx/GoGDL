34cf3296f5bb62a6e9c9c7d05904cf47b332dbce
Efrain Giron <efrainbond@hotmail.com>
2019-04-04 22:17:00 -0600

    Creada la funcionalidad de cerrar sesion

--- src/app/services/autorizacion.service.ts
+++ src/app/services/autorizacion.service.ts
@@ -1,23 +1,25 @@
 import {Injectable} from "@angular/core";
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AutorizacionService {
-  constructor(private angularFireAuth: AngularFireAuth, private afDB: AngularFireDatabase) {
+  constructor(private angularFireAuth: AngularFireAuth, private afDB: AngularFireDatabase, private router:Router) {
     this.isLogged();
-  }
+  };
   public login = (email, password) => {
     this.angularFireAuth.auth.signInWithEmailAndPassword(email, password)
     .then((response) => {
       alert('Usuario ingresado con exito');
       console.log(response);
+      this.router.navigate(['inicio']);
     })
     .catch((error) => {
       alert('Ah ocurrido un error');
       console.log(error);
     })
-  }
+  };
   public registro = (email, password, nombre, apellido) => {
     this.angularFireAuth.auth.createUserWithEmailAndPassword(email, password)
       .then((response) => {
@@ -28,6 +30,7 @@ export class AutorizacionService {
           Nombre: nombre,
           Apellido: apellido
         });
+        this.router.navigate(['inicio']);
       })
       .catch((error) => {
         console.log(error);
@@ -39,5 +42,10 @@ export class AutorizacionService {
   }
   public isLogged() {
     return this.angularFireAuth.authState;
+  };
+  public logout() {
+    this.angularFireAuth.auth.signOut();
+    alert("Sesión Cerrada");
+    this.router.navigate(['inicio']);
   }
 }
