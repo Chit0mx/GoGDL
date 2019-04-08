b696aaf11d72c50f8f09dc8e1b83a76a8d8606c0
Chit0mx <andreschit00@gmail.com>
2019-04-08 12:58:54 -0500

    About us

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,107 +1,112 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {FormsModule} from "@angular/forms";
-import {AgmCoreModule} from '@agm/core';
-import {ResaltarDirective} from './directives/resaltar.directive';
-import {ContarClicksDirective} from './directives/contar-clicks.directive';
-import {Routes, RouterModule} from '@angular/router';
-import{LugaresService} from "./services/lugares.service";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { AgmCoreModule } from "@agm/core";
+import { ResaltarDirective } from "./directives/resaltar.directive";
+import { ContarClicksDirective } from "./directives/contar-clicks.directive";
+import { Routes, RouterModule } from "@angular/router";
+import { LugaresService } from "./services/lugares.service";
 
 //Componentes
-import { LugaresComponent } from './lugares/lugares.component';
-import { DetalleComponent } from './detalle/detalle.component';
-import { ContactoComponent } from './contacto/contacto.component';
-import { CrearComponent } from './crear/crear.component';
-import { InicioComponent } from './inicio/inicio.component';
+import { LugaresComponent } from "./lugares/lugares.component";
+import { DetalleComponent } from "./detalle/detalle.component";
+import { ContactoComponent } from "./contacto/contacto.component";
+import { AboutComponent } from "./about/about.component";
+import { CrearComponent } from "./crear/crear.component";
+import { InicioComponent } from "./inicio/inicio.component";
 
-import { environment } from '../environments/environment';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFireStorageModule } from 'angularfire2/storage';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { DestacadosComponent } from './destacados/destacados.component';
-import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule }    from '@angular/common/http';
-import { AutorizacionService } from './services/autorizacion.service';
-import { GuardiaService } from './services/guardia.service';
-import { RegistroComponent } from './registro/registro.component';
+import { environment } from "../environments/environment";
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { AngularFireStorageModule } from "angularfire2/storage";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { DestacadosComponent } from "./destacados/destacados.component";
+import { InicioSesionComponent } from "./inicio-sesion/inicio-sesion.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { HttpClientModule } from "@angular/common/http";
+import { AutorizacionService } from "./services/autorizacion.service";
+import { GuardiaService } from "./services/guardia.service";
+import { RegistroComponent } from "./registro/registro.component";
 
 const appRoutes: Routes = [
-    {
-        path: '',
-        component: InicioComponent
-    }, 
-    {
-        path: 'inicio',
-        component: InicioComponent
-    }, 
-    {
-        path: 'destacados',
-        component: DestacadosComponent
-    }, 
-    {
-        path: 'lugares',
-        component: LugaresComponent
-    },
-    {
-        path: 'detalle/:id',
-        component: DetalleComponent
-    },
-    {
-        path: 'contacto',
-        component: ContactoComponent
-    },
-    {
-        path: 'crear',
-        component: CrearComponent,
-        canActivate:[GuardiaService]
-    },
-    {
-        path: 'inicio-sesion',
-        component: InicioSesionComponent
-    },
-    {
-        path: 'registro',
-        component: RegistroComponent
-    }
-
+  {
+    path: "",
+    component: InicioComponent
+  },
+  {
+    path: "inicio",
+    component: InicioComponent
+  },
+  {
+    path: "destacados",
+    component: DestacadosComponent
+  },
+  {
+    path: "lugares",
+    component: LugaresComponent
+  },
+  {
+    path: "detalle/:id",
+    component: DetalleComponent
+  },
+  {
+    path: "contacto",
+    component: ContactoComponent
+  },
+  {
+    path: "crear",
+    component: CrearComponent,
+    canActivate: [GuardiaService]
+  },
+  {
+    path: "inicio-sesion",
+    component: InicioSesionComponent
+  },
+  {
+    path: "registro",
+    component: RegistroComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  }
 ];
 @NgModule({
-    declarations: [
-        AppComponent,
-        ResaltarDirective,
-        ContarClicksDirective,
-        DetalleComponent,
-        LugaresComponent,
-        ContactoComponent,
-        CrearComponent,
-        InicioComponent,
-        DestacadosComponent,
-        InicioSesionComponent,
-        RegistroComponent
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        AngularFireModule,
-        AngularFirestoreModule,
-        AngularFireModule.initializeApp(environment.firebase),
-        AngularFireDatabaseModule,
-        AngularFireAuthModule,
-        AgmCoreModule.forRoot({
-          apiKey: 'AIzaSyAt_e0228SLA3GJe3XFha1CuLOmlLVrvxc'
-        }),
-        RouterModule.forRoot(appRoutes),
-        NgbModule.forRoot(),
-        HttpClientModule
-    ],
-    providers: [LugaresService, AutorizacionService, GuardiaService],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    ResaltarDirective,
+    ContarClicksDirective,
+    DetalleComponent,
+    LugaresComponent,
+    ContactoComponent,
+    AboutComponent,
+    CrearComponent,
+    InicioComponent,
+    DestacadosComponent,
+    InicioSesionComponent,
+    RegistroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyAt_e0228SLA3GJe3XFha1CuLOmlLVrvxc"
+    }),
+    RouterModule.forRoot(appRoutes),
+    NgbModule.forRoot(),
+    HttpClientModule
+  ],
+  providers: [LugaresService, AutorizacionService, GuardiaService],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/crear/crear.component.ts
+++ src/app/crear/crear.component.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
-import {LugaresService} from "../services/lugares.service";
+import { Component } from "@angular/core";
+import { LugaresService } from "../services/lugares.service";
 
 @Component({
-  selector: 'app-crear',
-  templateUrl: './crear.component.html'
+  selector: "app-crear",
+  templateUrl: "./crear.component.html"
 })
 export class CrearComponent {
-  lugar:any = {};
-  constructor(private lugaresService: LugaresService){}
-  guardarLugar(){
-    var direccion = this.lugar.calle + ',' + this.lugar.ciudad + ',' + this.lugar.pais;
-    this.lugaresService.obtenerGeoData(direccion)
-        .subscribe((result: any) => {
-          this.lugar.lat =  result.results[0].geometry.location.lat;
-          this.lugar.lng =  result.results[0].geometry.location.lng;
-          this.lugar.id = Date.now();
-          this.lugaresService.guardarLugar(this.lugar);
-          alert('Atraccion Guardada con exito');
-          this.lugar={};
-        });
+  lugar: any = {};
+  constructor(private lugaresService: LugaresService) {}
+  guardarLugar() {
+    var direccion =
+      this.lugar.calle + "," + this.lugar.ciudad + "," + this.lugar.pais;
+    this.lugaresService.obtenerGeoData(direccion).subscribe((result: any) => {
+      var str = new String();
+      str = result.status;
+      if (str == "ZERO_RESULTS" || str == "OVER_QUERY_LIMIT") {
+        alert("Dirección no encontrada.");
+      } else {
+        this.lugar.lat = result.results[0].geometry.location.lat;
+        this.lugar.lng = result.results[0].geometry.location.lng;
+        this.lugar.id = Date.now();
+        this.lugaresService.guardarLugar(this.lugar);
+        alert("Atraccion Guardada con exito");
+        this.lugar = {};
+      }
+    });
   }
 }
