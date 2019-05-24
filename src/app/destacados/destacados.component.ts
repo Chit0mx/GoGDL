3d3eaccb1ec7de2ce6b2b2d062f70de14c9e3d66
Efrain Giron <efrainbond@hotmail.com>
2019-05-24 11:45:02 -0500

    Creado filtro de calificación

--- src/app/destacados/destacados.component.ts
+++ src/app/destacados/destacados.component.ts
@@ -13,6 +13,10 @@ export class DestacadosComponent {
   lng: number = -103.3703034;
   lugares = null;
   articulos: any = {};
+  mC: Boolean = false;
+  listaCalificacion:any = [];
+  lPromedio = 0;
+  private listaCreada:Boolean = false;
 
   constructor(private lugaresService: LugaresService, private articulosService:ArticulosService) {
     lugaresService
@@ -30,4 +34,49 @@ export class DestacadosComponent {
   }
   filterLugar = "";
   filtroArt = "";
+
+  public mostrarCal(n1, n2, n3, n4, n5, lugar) {
+    if (this.listaCreada == false) {
+      let promedio = Math.round((5 * n5 + 4 * n4 + 3 * n3 + 2 * n2 + 1 * n1) / (n5 + n4 + n3 + n2 + n1)*1000)/1000;
+      lugar.promedio = promedio;
+      this.listaCalificacion.push(lugar);
+      this.listaCalificacion.sort((a, b) => {
+          if (a.promedio > b.promedio) {
+            return -1;
+          }
+          if (a.promedio < b.promedio) {
+            return 1;
+          }
+          return 0;
+        });
+      this.lPromedio = promedio;
+    } 
+  }
+
+  public mayorCalificacion(){
+    this.mC = !(this.mC);
+    this.listaCreada = true;
+  }
+
+  public range(start, stop, step) {
+    if (typeof stop == 'undefined') {
+        stop = start;
+        start = 0;
+    }
+
+    if (typeof step == 'undefined') {
+        step = 1;
+    }
+
+    if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
+        return [];
+    }
+
+    var result = [];
+    for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
+        result.push(i);
+    }
+
+    return result;
+  }
 }
