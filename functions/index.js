a96cb83c42ae403c240bcd0d1ae4f69f4acb3882
Efrain Giron <efrainbond@hotmail.com>
2019-05-25 19:34:12 -0500

    Avance de enviar emails

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions');
+
+const sendgrid = require('sendgrid');
+const cliente = sendgrid("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");
+
+function parseBody()
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
