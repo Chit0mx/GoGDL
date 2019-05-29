f7a7be9de06144cacfd885a5d3cf216d2380e23b
Efrain Giron <efrainbond@hotmail.com>
2019-05-29 00:22:51 -0500

    Completas las notificaciones solo falta pulirlas

--- functions/index.js
+++ functions/index.js
@@ -9,29 +9,38 @@ exports.fcmSend = functions.database.ref("/articulos/{articuloId}").onCreate((sn
     snapshot.forEach(datos => {
       let usuarioId = datos.key;
       let usuario = datos.val();
-      if (usuario[lugarId].favorito) {
-        admin.database().ref(`/lugares/${lugarId}`).once('value').then(snapshot => {
-          let lugarNombre = snapshot.val().nombre;
-          let payload = {
-            notifications : {
-              title: `Hay un nuevo evento/promocion en ${lugarNombre}`,
-              body: `Ve y hechale un vistaso en https://gogdl-1551633194886.firebaseapp.com/detalle/${lugarId}`,
-              icon: 'https://www.armytimes.com/resizer/EMlsFi9L9EXZkrjnDO5GMwcpWHo=/1200x0/filters:quality(100)/arc-anglerfish-arc2-prod-mco.s3.amazonaws.com/public/5RAPSCITXBGULPO7VZSOXJGWI4.jpg'
-            }
-          };
-          admin.database(`/fcmTokens/${usuarioId}`).once('value')
-          .then(token => token.val()).then(userFcmToken => {
-            return admin.messaging().sendToDevice(userFcmToken, payload);
-          }).then(res => {
-            console.log("Envio de notificacion exitoso", res);
+      console.log(usuario);
+      if (usuario[lugarId]){
+        let lugar = usuario[lugarId]
+        if (lugar.favorito) {
+          admin.database().ref(`/lugares/${lugarId}`).once('value').then(snapshot => {
+            let lugarNombre = snapshot.val().nombre;
+            let payload = {
+              notification : {
+                title: `Hay un nuevo evento/promocion en ${lugarNombre}`,
+                body: `Ve y hechale un vistaso en https://gogdl-1551633194886.firebaseapp.com/detalle/${lugarId}`,
+                icon: 'https://www.armytimes.com/resizer/EMlsFi9L9EXZkrjnDO5GMwcpWHo=/1200x0/filters:quality(100)/arc-anglerfish-arc2-prod-mco.s3.amazonaws.com/public/5RAPSCITXBGULPO7VZSOXJGWI4.jpg'
+              }
+            };
+            admin.database().ref(`/fcmTokens/${usuarioId}`).once('value')
+            .then(token => token.val()).then(userFcmToken => {
+              console.log(`Mensaje enviado`)
+              return admin.messaging().sendToDevice(userFcmToken, payload);
+            }).then(res => {
+              console.log("Envio de notificacion exitoso", res);
+            }).catch(error => {
+              console.log("El envio no fue exitoso", error)
+            });
           }).catch(error => {
-            console.log("El envio no fue exitoso", error)
-          });
-        }).catch(error => {
-            console.log(`${error} No se pudo traer al usuario en la funcion de firebase`)        
-          });
+              console.log(`${error} No se pudo traer al lugar en la funcion de firebase`)        
+            });
+        } else {
+          console.log(`${usuario.Nombre} no tienen esta atracción como favorita`)
+          return 0;
+        }
       } else {
-        return;
+        console.log(`${usuario.Nombre} no a visto esta atracción`);
+        return 0;
       }
     });
   }).catch(error => {
