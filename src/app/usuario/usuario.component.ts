79a90ec7c1910d51187aed1377bab4d2ffac186f
Chit0mx <andreschit00@gmail.com>
2019-05-30 19:22:23 -0500

    Performance version v1

--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -1,67 +1,67 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
-import{ LugaresService }from "../services/lugares.service";
-import { AutorizacionService } from '../services/autorizacion.service';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { Component, ChangeDetectorRef } from "@angular/core";
+import { LugaresService } from "../services/lugares.service";
+import { AutorizacionService } from "../services/autorizacion.service";
+import { AngularFireAuth } from "angularfire2/auth";
+import { AngularFireDatabase } from "angularfire2/database";
 import swal from "sweetalert2";
-import { Router } from '@angular/router';
-import { GeoLocationService } from '../services/geolocation.service';
-
+import { Router } from "@angular/router";
+import { GeoLocationService } from "../services/geolocation.service";
 @Component({
-  selector: 'app-usuario',
-  templateUrl: './usuario.component.html'
+  selector: "app-usuario",
+  templateUrl: "./usuario.component.html"
 })
-
 export class UsuarioComponent {
-  title = 'GoGdl';
-  lugares:any;
-  lugaresOcultos:any;
-  loggedUser:any = null;
-  usuarioDB:any;
+  title = "GoGdl";
+  lugares: any;
+  lugaresOcultos: any;
+  loggedUser: any = null;
+  usuarioDB: any;
   //latU:number;
   //lngU:number;
-  cordinates:any;
+  cordinates: any;
   promedios: any = [];
   promedio = 0;
   /*options = {
     timeout : 500,
     maximumAge: 0
   };*/
-  errorMsg: string; 
+  errorMsg: string;
   currentLocation: any = null;
 
-  constructor(private afDB: AngularFireDatabase,
+  constructor(
+    private afDB: AngularFireDatabase,
     private angularFireAuth: AngularFireAuth,
-    private lugaresService: LugaresService, 
-    private autorizacionService:AutorizacionService,
+    private lugaresService: LugaresService,
+    private autorizacionService: AutorizacionService,
     private router: Router,
     private ref: ChangeDetectorRef,
     private geoLocationService: GeoLocationService
-    ) {
+  ) {
     lugaresService
-    .getLugares()
-    .valueChanges()
-    .subscribe(lugares => {
-      this.lugares = lugares;
-    });
+      .getLugares()
+      .valueChanges()
+      .subscribe(lugares => {
+        this.lugares = lugares;
+      });
 
     lugaresService
-    .getLugaresOcultos()
-    .valueChanges() 
-    .subscribe(lugaresO => {
-      this.lugaresOcultos = lugaresO;
-    });
+      .getLugaresOcultos()
+      .valueChanges()
+      .subscribe(lugaresO => {
+        this.lugaresOcultos = lugaresO;
+      });
 
     this.loggedUser = this.autorizacionService.getUser().currentUser.uid;
-    this.autorizacionService.obtenerUsuario()
-    .valueChanges().
-    subscribe(usuarioDB => {
-      this.usuarioDB = usuarioDB;
-    });
+    this.autorizacionService
+      .obtenerUsuario()
+      .valueChanges()
+      .subscribe(usuarioDB => {
+        this.usuarioDB = usuarioDB;
+      });
     this.searchByCurrent();
   }
 
-  public bajaUser(){
+  public bajaUser() {
     this.autorizacionService.bajaUsuario();
     this.router.navigate([`/inicio`]);
   }
@@ -69,64 +69,75 @@ export class UsuarioComponent {
     this.autorizacionService.hacerEmpresario();
   }
 
-  public desocultar(lugar){
-    swal.fire({
-      title: '¿Esta seguro que desea dejar de ocultar esta atracción?',
-      text: "Volvera a estar disponible para todos los usuarios de GoGDL",
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Si, dejar de ocultar'
-    }).then((result) => {
-      if (result.value) {
-        this.lugaresService.desocultarLugar(lugar);
-        this.router.navigate([`/detalle/${lugar.id}`]);
-        swal.fire(
-          "La atracción ya no esta oculta",
-          "Ahora esta disponible para todos los usuarios",
-          "info"
-        );
-      }
-    })
+  public desocultar(lugar) {
+    swal
+      .fire({
+        title: "¿Esta seguro que desea dejar de ocultar esta atracción?",
+        text: "Volvera a estar disponible para todos los usuarios de GoGDL",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, dejar de ocultar"
+      })
+      .then(result => {
+        if (result.value) {
+          this.lugaresService.desocultarLugar(lugar);
+          this.router.navigate([`/detalle/${lugar.id}`]);
+          swal.fire(
+            "La atracción ya no esta oculta",
+            "Ahora esta disponible para todos los usuarios",
+            "info"
+          );
+        }
+      });
   }
 
   public mostrarCal(n1, n2, n3, n4, n5) {
-    this.promedio = Math.round((5 * n5 + 4 * n4 + 3 * n3 + 2 * n2 + 1 * n1) / (n5 + n4 + n3 + n2 + n1)*1000)/1000;
+    this.promedio =
+      Math.round(
+        ((5 * n5 + 4 * n4 + 3 * n3 + 2 * n2 + 1 * n1) /
+          (n5 + n4 + n3 + n2 + n1)) *
+          1000
+      ) / 1000;
     this.promedios.push(this.promedio);
   }
 
   public range(start, stop, step) {
-    if (typeof stop == 'undefined') {
-        stop = start;
-        start = 0;
+    if (typeof stop == "undefined") {
+      stop = start;
+      start = 0;
     }
 
-    if (typeof step == 'undefined') {
-        step = 1;
+    if (typeof step == "undefined") {
+      step = 1;
     }
 
     if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
-        return [];
+      return [];
     }
 
     var result = [];
     for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
-        result.push(i);
+      result.push(i);
     }
 
     return result;
   }
 
-  searchByCurrent() { let self = this;
-    const accuracy = { enableHighAccuracy: true }; 
-    self.geoLocationService.getLocation(accuracy).subscribe((position) => {
-    console.log(position);
-    self.currentLocation = position; 
-    self.ref.detectChanges();
-    }, (error) => { 
-      self.errorMsg = error;
-      self.ref.detectChanges(); 
-    } );
+  searchByCurrent() {
+    let self = this;
+    const accuracy = { enableHighAccuracy: true };
+    self.geoLocationService.getLocation(accuracy).subscribe(
+      position => {
+        console.log(position);
+        self.currentLocation = position;
+        self.ref.detectChanges();
+      },
+      error => {
+        self.errorMsg = error;
+        self.ref.detectChanges();
+      }
+    );
   }
 }
